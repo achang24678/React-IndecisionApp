3fb267e06560eb144ededbb4202f37d1c8071261
Allen Chang <achang24678@gmail.com>
2019-03-26 21:21:52 -0700

    testing commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 
-const Header = (props) => (
+const Header = (props) => ( 
     <div className="header">
         <div className="container">
             <h1 className="header__title">{props.title}</h1>
